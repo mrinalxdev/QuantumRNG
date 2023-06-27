c770edb14a39fdba3e78fa6db0d60e79adb86730
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-27 20:59:18 +0530

    Added Basic Layout for the project

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,23 +1,53 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
     "a b f"
     "d e f"
     "d e f"
-    "d g i"
+    "d h i"
     "g h i"
     "g h j"
     "g h j"
+`;
 
-
-
-
-`
+const gridTemplateSmallScreens = `
+"a"
+"a"
+"a"
+"a"
+"b"
+"b"
+"b"
+"b"
+"c"
+"c"
+"c"
+"d"
+"d"
+"d"
+"e"
+"e"
+"f"
+"f"
+"f"
+"g"
+"g"
+"g"
+"h"
+"h"
+"h"
+"h"
+"i"
+"i"
+"j"
+"j"
+`;
 
 const Dashboard = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   const { palette } = useTheme();
   return (
     <Box
@@ -25,11 +55,49 @@ const Dashboard = () => {
       height="100%"
       display="grid"
       gap="1.5rem"
-      sx={{
-        gridTemplateColumns : "repeat(3, minmax(370px, 1fr))",
-        gridTemplateAreas: gridTemplate,
-      }}
-    ></Box>
+      sx={ 
+        isAboveMediumScreens ?  {
+        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+        gridTemplateAreas: gridTemplateLargeScreens,
+      } : {
+        gridAutoColumns: "1fr",
+        gridAutoRows: "80px",
+        gridTemplateAreas : gridTemplateSmallScreens,
+      }
+    }
+    >
+      <Box gridArea="a" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="b" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="c" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="d" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="e" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="f" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="g" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="h" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="i" bgcolor="#fff">
+        Hello
+      </Box>
+      <Box gridArea="j" bgcolor="#fff">
+        Hello
+      </Box>
+    </Box>
   );
 };
 
