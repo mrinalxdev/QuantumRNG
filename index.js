fac3af5e9f55f628dcdcfaba147b70946dfa129c
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-12-12 03:14:54 +0530

    Added New QuantumErrorConenction

--- index.js
+++ index.js
@@ -236,6 +236,50 @@ class QuantumCommunication {
     return message;
   }
 }
+//FOR THE ERROR HANDELING
+class QuantumErrorConnection {
+  static runShorCode(qubits){
+    const errorProbability = 0.1
+    const noisyQubits = qubits.map(qubit => {
+      return Math.random() < errorProbability ? 1 - qubit : qubit;
+    })
+
+    const correctedQubit = [];
+    for (let i = 0; i < noisyQubits.length; i++){
+      const neighbors = this.getNeighbors(i, noisyQubits.length)
+      const majorityVote = this.calculationMajorityVote(neighbors, noisyQubits)
+
+      correctedQubit.push(majorityVote)
+    }
+
+    return correctedQubit
+  }
+
+  static getNeighbors(index, length){
+    const prevIndex = (index - 1 + length) % length
+    const nextIndex = (index + 1) % length
+    return [prevIndex, nextIndex, index]
+  }
+
+
+  static calculationMajorityVote(neighbors, qubits){
+    const voteCount = { 0: 0, 1: 0}
+
+    for(const neighbor of neighbors){
+      const value = qubits[neighbor]
+      voteCount[value]++;
+    }
+
+    return voteCount[0] > voteCount[1] ? 0 : 1;
+  }
+
+
+  static visualizeErrorConnection(originalQubits, noisyQubits, correctQubits) {
+    console.log('Original Qubits: ', originalQubits )
+    console.log('Noisy Qubits: ', noisyQubits )
+    console.log('Corrected Qubits: ', correctQubits )
+  }
+}
 
 class QuantumAlgorithm {
   static runQuantumEntanglement(circuit, qubit1, qubit2) {
@@ -303,7 +347,8 @@ module.exports = {
   QuantumAnimator,
   QuantumEntanglement,
   QuantumCircuit,
-  QuantumCircuit,
+  QuantumErrorConnection,
+  QuantumAlgorithm,
   QuantumGate,
   QuantumTeleportation,
   QuantumCommunication,
