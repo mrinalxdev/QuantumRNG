0f04b8487395a2929d83f4cc71c2271eaa908b5a
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-12-11 17:13:53 +0530

    Added QuantumAnimation Engine and QuantumEntanglement Engine

---
+++ README.md
@@ -0,0 +1,4 @@
+# QuantumRNG a Node Package Manager [![Developing Time](https://wakatime.com/badge/user/6e3553f3-7d6a-4619-af68-505157a93d06/project/018c546b-0d0f-4ad0-8f0b-decd4b5eb52a.svg)](https://wakatime.com/badge/user/6e3553f3-7d6a-4619-af68-505157a93d06/project/018c546b-0d0f-4ad0-8f0b-decd4b5eb52a)
+
+
+
--- index.js
+++ index.js
@@ -111,3 +111,105 @@ class QuantumState {
     }
   }
 }
+
+class QuantumAnimator {
+  constructor(element) {
+    this.element = element;
+  }
+
+  superpostionAnimation({ duration, property, values, easing = "linear" }) {
+    const startTime = Date.now();
+    const easeFunctions = {
+      linear: (t) => t,
+      easeInQuad: (t) => t * t,
+      easeOutQuad: (t) => t * (2 - t),
+      easeInOutQuad: (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+    };
+
+    const ease = easeFunctions[easing] || easeFunctions.linear;
+
+    const animate = () => {
+      const currentTime = Date.now();
+      const progress = Math.min(1, (currentTime - startTime) / duration);
+
+      const interpolatedValue =
+        values[0] + ease(progress * (values[1] - values[0]));
+      this.element.style[property] = interpolatedValue;
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      }
+    };
+
+    animate();
+  }
+
+  quantumGateAnimation({duration, gates}){
+    const startTime = Date.now();
+    let currentState = 0;
+
+
+    const animate = () => {
+      const currentTime = Date.now()
+      const progress = Math.min(1, (currentTime - startTime) / duration)
+
+      currentState = Math.floor(progress * gates.length)
+      this.element.innerHTML = gates[currentState]
+
+
+      if (progress < 1){
+        requestAnimationFrame(animate)
+      }
+    }
+
+    animate();
+  }
+
+  entangledAnimation({duration, property, values, entangledElements}){
+    const startTime = Date.now()
+    const animate = () => {
+      const currentTime = Date.now()
+      const progress = Maths.min(1, (currentTime - startTime) / duration)
+
+      const interpolatedValue = values[0] + progress * (values[1] - values[0])
+      this.element.style[property] = interpolatedValue
+ 
+      entangledElements.forEach((entangledElement) => {
+        entangledElement.style[property] = interpolatedValue
+      })
+
+      if (progress < 1){
+        requestAnimationFrame(animate)
+      }
+    }
+
+    animate()
+  }
+}
+
+class QuantumEntanglement {
+  constructor() {
+    this.variables = {};
+  }
+
+  createVariable(initialValue){
+    const variable = {
+      value : initialValue,
+      entangledVariables : [],
+      set : function (newValue) {
+        this.value = newValue; 
+        this.entangledVariables.forEach((entangledVar) => {
+          entangledVar.value = newValue;
+        })
+      },
+      
+      entangle: function (otherVariable) {
+        this.entangledVariables.push(otherVariable)
+        otherVariable.entangledVariables.push(this)
+      }
+    }
+
+    this.variables[variable.value] = variable
+    return variable;
+  }
+}
